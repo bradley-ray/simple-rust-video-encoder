4a936a8a37ff8dccad121eb0cd0e0f874eb47f78
Bradley Ray <bradleyr320@gmail.com>
2023-01-03 13:42:32 -0600

    move some stuff to functions and return io::Result<T>s

--- main.rs
+++ main.rs
@@ -1,14 +1,31 @@
 use std::env;
 use std::fs;
 
+use std::io;
 use std::io::{Read, BufReader};
 use std::io::{Write, BufWriter};
 
 const MB: f32 = 1_000_000.;
 
+type Frame = Vec<u8>;
+
+struct ColorRGB{r: f32, g: f32, b: f32}
+struct ColorYUV{y: f32, u: f32, v: f32}
+
+impl ColorRGB {
+    fn to_yuv(&self) -> ColorYUV {
+        let y =  0.299*self.r + 0.587*self.g + 0.114*self.b;
+        let u = -0.169*self.r - 0.331*self.g + 0.449*self.b  + 128.0;
+        let v =  0.499*self.r - 0.418*self.g - 0.0813*self.b + 128.0;
+        
+        ColorYUV{y, u, v}
+    }
+}
+
+
 // TODO: currently just lazily using unwrap, so need to
 // properly handle the Result<> later
-fn main() {
+fn main() -> io::Result<()> {
     // TODO: make/use better tooling for cmd line args
     let args: Vec<String> = env::args().collect();
     let in_path = args.get(1).unwrap();
@@ -23,20 +40,24 @@ fn main() {
 
     // Read video file into memory
     let buffer_size = width*height*3;
-    let frames = read_video(in_path, buffer_size);
+    let frames = read_video(in_path, buffer_size)?;
     println!("number of frames: {}", frames.len());
     println!();
 
     let raw_size = video_size(frames.len(), buffer_size);
     println!("(original rgb) size: {} MB\n", raw_size);
 
-    let yuv_frames = yuv_encode(&frames, buffer_size, width, height);
+    // convert to yuv and downsample
+    let yuv_frames = yuv_encode(&frames, width, height);
     let yuv_size = video_size(yuv_frames.len(), yuv_frames.get(0).unwrap().len());
+
     println!("(converted yuv) size: {} MB", yuv_size);
     println!("(converted yuv) yuv/rgb: {} %\n", 100.0 * yuv_size / raw_size);
 
+
     // Write encoded video to file
-    write_video(out_path, &yuv_frames);
+    // TODO: write rle encoder
+    write_video(out_path, &yuv_frames)
 }
 
 // get video size in MB
@@ -44,14 +65,14 @@ fn video_size(len: usize, buffer_size: usize) -> f32 {
     (len * buffer_size) as f32 / MB
 }
 
-// TODO: look into using a Result<T> as the return instead
-fn read_video(path: &str, buffer_size: usize) ->  Vec<Vec<u8>> {
+// read video into memory from input file
+fn read_video(path: &str, buffer_size: usize) ->  io::Result<Vec<Frame>> {
     let mut frames = Vec::new();
-    let file = fs::File::open(path).unwrap();
+    let file = fs::File::open(path)?;
     let mut reader = BufReader::new(file);
     loop {
         let mut frame_buffer = vec![0; buffer_size];
-        let n = reader.read(&mut frame_buffer).unwrap();
+        let n = reader.read(&mut frame_buffer)?;
         if n < frames.len() {
             break;
         }
@@ -59,60 +80,71 @@ fn read_video(path: &str, buffer_size: usize) ->  Vec<Vec<u8>> {
         frames.push(frame_buffer);
     }
 
-    frames
+    Ok(frames)
 }
 
-// TODO: again, look into using Result<T> as return type
-fn write_video(path: &str, frames: &Vec<Vec<u8>>) {
+// write vidoe to output file
+fn write_video(path: &str, frames: &Vec<Frame>) -> io::Result<()> {
     let file = fs::File::options()
                         .write(true)
                         .create(true)
-                        .open(path).unwrap();
+                        .open(path)?;
     let mut writer = BufWriter::new(file);
-    for frame in frames {
-        writer.write(frame).unwrap();
+    writer.write_all(&frames.concat())
+}
+
+// convert single frame from rgb to yuv
+fn frame_to_yuv(frame: &Frame, y_buf: &mut Frame, u_buf: &mut Vec<f32>, v_buf: &mut Vec<f32>, size: usize) {
+    for n in 0..size {
+        let r = frame[3*n] as f32;
+        let g = frame[3*n+1] as f32;
+        let b = frame[3*n+2] as f32;
+
+        let ColorYUV{y, u, v} = ColorRGB{r, g, b}.to_yuv();
+
+        y_buf.push(y as u8);
+        u_buf.push(u);
+        v_buf.push(v);
     }
 }
 
-// TODO: this can probably be improved once a learn a bit more about rust
-fn yuv_encode(frames: &Vec<Vec<u8>>, buffer_size: usize, width: usize, height: usize) -> Vec<Vec<u8>>{
+// downsample u & v
+fn uv_downsample(u_buf: &Vec<f32>, v_buf: &Vec<f32>, 
+                    u_down_buf: &mut Frame, v_down_buf: &mut Frame, 
+                    width: usize, height: usize) {
+    for x in (0..height).step_by(2) {
+        for y in (0..width).step_by(2) {
+            let u = (u_buf[x*width+y] + u_buf[x*width+y+1] + u_buf[(x+1)*width+y] + u_buf[(x+1)*width+y+1]) / 4.0;
+            let v = (v_buf[x*width+y] + v_buf[x*width+y+1] + v_buf[(x+1)*width+y] + v_buf[(x+1)*width+y+1]) / 4.0;
+
+            u_down_buf[x/2*width/2+y/2] = u as u8;
+            v_down_buf[x/2*width/2+y/2] = v as u8;
+        }
+    }
+}
+
+fn yuv_encode(frames: &Vec<Frame>, width: usize, height: usize) -> Vec<Frame>{
     let mut yuv_frames = Vec::new();
+
     for frame in frames {
-        let mut y: Vec<u8>  = vec![0; buffer_size/3];
-        let mut u: Vec<f32> = vec![0.0; buffer_size/3];
-        let mut v: Vec<f32> = vec![0.0; buffer_size/3];
+        let mut y_buf: Frame    = Vec::with_capacity(width*height);
+        let mut u_buf: Vec<f32> = Vec::with_capacity(width*height);
+        let mut v_buf: Vec<f32> = Vec::with_capacity(width*height);
 
-        for n in 0..y.len() {
-            let r = *frame.get(3*n).unwrap() as f32;
-            let g = *frame.get(3*n+1).unwrap() as f32;
-            let b = *frame.get(3*n+2).unwrap() as f32;
+        // calculate y, u, v values
+        let size = y_buf.capacity();
+        frame_to_yuv(frame, &mut y_buf, &mut u_buf, &mut v_buf, size);
 
-            let y_ =  0.299*r + 0.587*g + 0.114*b;
-            let u_ = -0.169*r - 0.331*g + 0.449*b  + 128.0;
-            let v_ =  0.499*r - 0.418*g - 0.0813*b + 128.0;
+        // average pixels together
+        let mut u_downsampled = vec![0; width*height/4];
+        let mut v_downsampled = vec![0; width*height/4];
+        uv_downsample(&u_buf, &v_buf, &mut u_downsampled, &mut v_downsampled, width, height);
 
-            y[n] = y_ as u8;
-            u[n] = u_;
-            v[n] = v_;
-        }
-
-        let mut u_downsampled = vec![0; buffer_size/3/4];
-        let mut v_downsampled = vec![0; buffer_size/3/4];
-        for x in (0..height).step_by(2) {
-            for y in (0..width).step_by(2) {
-                let u_ = (u.get(x*width+y).unwrap() + u.get(x*width+y+1).unwrap() + u.get((x+1)*width+y).unwrap() + u.get((x+1)*width+y+1).unwrap()) / 4.0;
-                let v_ = (v.get(x*width+y).unwrap() + v.get(x*width+y+1).unwrap() + v.get((x+1)*width+y).unwrap() + v.get((x+1)*width+y+1).unwrap()) / 4.0;
-
-                u_downsampled[x/2*width/2+y/2] = u_ as u8;
-                v_downsampled[x/2*width/2+y/2] = v_ as u8;
-            }
-        }
-
-        let mut yuv_frame_buffer  = Vec::new();
-        yuv_frame_buffer.append(&mut y);
+        let mut yuv_frame_buffer  = Vec::with_capacity(frame.len());
+        yuv_frame_buffer.append(&mut y_buf);
         yuv_frame_buffer.append(&mut u_downsampled);
         yuv_frame_buffer.append(&mut v_downsampled);
-
+    
         yuv_frames.push(yuv_frame_buffer);
     }
 
